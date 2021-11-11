9462e769b1db89f76ed59c79a3521a9d1062ef92
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-11 13:43:50 +0900

    done with lists (3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create] do
+    member do
+      resources :bookmarks, only: [:new, :create]
+    end
+  end
 end
