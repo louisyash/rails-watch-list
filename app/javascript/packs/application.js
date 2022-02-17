434da0a9cb0fbd72f69adef4edab494d7b74aa44
Louis Yasuhiro <lyasuhir@gmail.com>
2022-02-17 11:17:46 +0900

    fixed stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
+console.log("Hello from app/javascript/packs/application.js!");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,3 +18,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
